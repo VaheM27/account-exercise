d975b3e6c708468f5c5c5a0d998fd29667abfa02
VaheM27 <vmnatsakanyan27@gmail.com>
2022-04-13 20:15:42 +0400

    formated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,22 +11,13 @@ export class DataService {
     'https://mockend.com/rawmediamarketing/vue-assignment-api/accounts';
   userUrl = '';
   accounts: any;
-  account:any
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
-    this.route.params.subscribe((params) => {
-      this.userUrl = `https://mockend.com/rawmediamarketing/vue-assignment-api/accounts/${params['id']}`;
-    });
-  }
+  constructor(
+    private http: HttpClient, 
+    private route: ActivatedRoute
+    ) { }
 
   async getData() {
     const res = await firstValueFrom(this.http.get(this.configUrl));
     return res;
-  }
-
-  async getUserData() {
-    const result = await firstValueFrom(this.http.get(this.userUrl));
-    console.log(this.userUrl);
-    
-    return result;
   }
 }
